aa3b48f214af934e84f7e225af3ec495c78e33ed
zack10 <zacksmetz@yahoo.com>
2024-05-18 05:52:50 +0200

    Configure database for Heroku

--- db.js
+++ db.js
@@ -11,6 +11,10 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
