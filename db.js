4ca21b84a2b7e931f291812a53768ec3ad7e5b35
zack10 <zacksmetz@yahoo.com>
2024-05-18 06:11:32 +0200

    the db code open

--- db.js
+++ db.js
@@ -1,20 +1,14 @@
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: "postgres",
+  host: "localhost",
+  database: "blog",
+  password: "ZzAa00$$",
+  port: 5432,
 });
 
 export default pool;
+
+//first in branch
