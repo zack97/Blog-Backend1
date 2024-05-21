6d12d9bd981a028a45afde2f7937552f602aa7ae
zack10 <zacksmetz@yahoo.com>
2024-05-21 08:09:36 +0200

    commit to heroku adding database url

--- db.js
+++ db.js
@@ -8,13 +8,11 @@ const { Pool } = pkg;
 let pool;
 
 if (process.env.DATABASE_URL) {
-  // If DATABASE_URL is available (i.e., on Heroku), use it for connection
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }, // Required for Heroku PostgreSQL
   });
 } else {
-  // If DATABASE_URL is not available (i.e., on local machine), use environment variables
   pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
