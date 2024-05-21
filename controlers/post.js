2d2bd26a747d91a5544692b132d4438634322410
zack10 <zacksmetz@yahoo.com>
2024-05-21 11:12:03 +0200

    Update database schema again1

--- controlers/post.js
+++ controlers/post.js
@@ -41,7 +41,6 @@ export const getPost = (req, res) => {
   });
 };
 
-//
 export const addPost = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not authenticated!");
@@ -49,19 +48,24 @@ export const addPost = (req, res) => {
   jwt.verify(token, "zackKey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-    const q = "INSERT INTO posts(title,description,img,cat,date,uid) VALUES(?)";
+    const q = `
+      INSERT INTO posts (title, description, img, cat, uid)
+      VALUES ($1, $2, $3, $4, $5)
+    `;
 
     const values = [
       req.body.title,
       req.body.description,
       req.body.img,
       req.body.cat,
-      req.body.date,
-      userInfo.id,
+      userInfo.id, // Use authenticated user's ID
     ];
 
-    pool.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
+    pool.query(q, values, (err, data) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json("Internal Server Error");
+      }
       return res.json("Post has been created");
     });
   });
