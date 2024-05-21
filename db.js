b7ba215eb26d21e497ab0c5e44af0a3f4c875455
zack10 <zacksmetz@yahoo.com>
2024-05-21 06:58:11 +0200

    commit to heroku 4

--- db.js
+++ db.js
@@ -5,12 +5,23 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // If DATABASE_URL is available (i.e., on Heroku), use it for connection
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Required for Heroku PostgreSQL
+  });
+} else {
+  // If DATABASE_URL is not available (i.e., on local machine), use environment variables
+  pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+}
 
 export default pool;
